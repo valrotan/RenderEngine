1508d147e9d35a0a91afdf56cc6893ef81a3af4b
TarasPriadka <taras.priadka@gmail.com>
2020-03-05 13:38:15 -0800

    Commented and cleaned parsing code

--- RenderEngine/src/parsing/objParser.c
+++ RenderEngine/src/parsing/objParser.c
@@ -4,22 +4,7 @@
 #include <string.h>
 #include "linkedList.h"
 
-// helper functions
 
-FILE *openFile(char *path);
-void parseFaceLine(char *str, int *vertN, int **verts);
-
-
-/*
-	Parses an obj file and outputs triangles that is interpretable by the program
-	PRE:	path - path to the .obj file
-			trigList - a pointer to the triangle list
-			size - output int for the number of triangles
-			scale - the scale factor to scale all of the models to be of similar size
-	POST:
-			File is parsed and all of the pre variables are set properly
-	returns: void
-*/
 void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 	FILE *fpIn = openFile(path);
 
@@ -33,58 +18,63 @@ void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 
 	int sizeFaces = 1000;
 	StackNode* facesStack = (StackNode*)malloc(sizeof(StackNode));
-	Vector3D* centerElement = (Vector3D*)malloc(sizeof(Vector3D));
+	Vector3D* centerElement = (Vector3D*)malloc(sizeof(Vector3D)); // used for centering vertecies
 	centerElement->x = 0;
 	centerElement->y = 0;
 	centerElement->z = 0;
 
-	//Triangle3D *faces = (Triangle3D *)malloc(sizeFaces * sizeof(Triangle3D));
-	//Triangle3D *tempFaces;
 	int facesCount = 0;
 
 	while (fscanf(fpIn, "%s", line) != EOF) {
-		// printf("\'%s\'\n",line);
+		
+		//skip comment lines in the file
 		if (strstr(line, "#")) {
 			fscanf(fpIn, "%[^\n]s", line);
 		}
+
+		//capture path to the mtl file
 		if (strstr(line, "mtllib")) {
 			fscanf(fpIn, "%s", line);
 			strcpy(materialsPath, line);
 		}
+
+		//capturing data lines
 		if (strlen(line) == 1) {
+
+			//skipping object line
 			if (strstr(line, "o")) {
 				fscanf(fpIn, "%s", line);
-				// printf("LINE: %s\n", line);
+
+			// capturing vertecies
 			} else if (line[0] == 'v' &&
-								 line[1] == '\0') { // strstr(line, "v") && !strstr(line,"vn")
-				if (vertCount >= sizeVerts) {
+								 line[1] == '\0') {
+				//checking if need to expand the size of vertecies if reached max
+				if (vertCount >= sizeVerts) { 
 					sizeVerts *= 2;
 					Vector3D *temp = realloc(verts, sizeVerts * sizeof(Vector3D));
 					if (!temp) {
-						printf("SOME ERROR\n");
+						printf("Error, wasn't able to reallocate verts!\n");
 					}
 					verts = temp;
 				}
+
 				float x, y, z;
 				fscanf(fpIn, "%f %f %f", &x, &y, &z);
-				// printf("GOT: %f %f %f\n", x, y, z);
 				Vector3D a = {x, y, z};
 				add(centerElement, &a, centerElement);
 				verts[vertCount] = a;
 				vertCount++;
+			
+			// parse faces
 			} else if (line[0] == 'f' && line[1] == '\0') {
-				//if (facesCount + 10 >= sizeFaces) {
-				//	sizeFaces *= 2;
-				//	tempFaces = realloc(faces, sizeFaces * sizeof(Triangle3D));
-				//	if (!tempFaces) {
-				//		printf("Error with allocation!\n");
-				//	}
-				//	faces = tempFaces;
-				//}
 				fscanf(fpIn, "%[^\n]s", line);
-				// printf("%s\n", line);
+
 				int *indexes, vertNum;
+
+				// parsing the face line to create multiple triangles if more
+				// than 3 vertecies
 				parseFaceLine(line, &vertNum, &indexes);
+
 				int first = indexes[0];
 				for (int i = 1; i + 1 < vertNum; i++) {
 					Vector3D *aa = verts + first - 1;
@@ -100,9 +90,9 @@ void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 							0.25f,
 							.25f,
 							.1f};
+
 					Triangle3D* pTemp = (Triangle3D*)malloc(sizeof(Triangle3D));
 					*pTemp = trig;
-					//faces[facesCount] = trig;
 					facesStack = push(facesStack, pTemp);
 					facesCount++;
 				}
@@ -110,17 +100,11 @@ void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 			}
 		}
 	}
-
-	//memory wasted 814kb
-	// printf("VERTC: %d\n", vertCount);
-	// printf("FACEC: %d\n", facesCount);
-	
 	Triangle3D* triggs = (Triangle3D*)malloc(facesCount * sizeof(Triangle3D));
-	
 	int i = 0;
 
-	divide(centerElement, vertCount, centerElement);
-	
+	divide(centerElement, vertCount, centerElement); // calculate average x,y,z
+
 	float xMax = 0, xMin = 0;
 	float yMax = 0, yMin = 0;
 	float zMax = 0, zMin = 0;
@@ -132,6 +116,7 @@ void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 		setMinMax(&(verts[i].y), &yMin, &yMax);
 		setMinMax(&(verts[i].z), &zMin, &zMax);
 	}
+
 
 	xMax = xMax - xMin;
 	yMax = yMax - yMin;
@@ -177,12 +162,6 @@ void parseObj(char *path, Triangle3D **trigList, int *size, float *scale) {
 }
 
 
-
-void centerVertecies(Vector3D* verts) {
-
-}
-
-
 void parseFaceLine(char *line, int *vertNumber, int **v) {
 	int spaces = 0;
 	for (int i = 0; line[i] != '\0'; i++) {
@@ -203,14 +182,6 @@ void parseFaceLine(char *line, int *vertNumber, int **v) {
 		}
 
 		if (strstr(temp, "/")) {
-			// char* aStr = parser_strtok_r(line, "/", 1);
-			// fscanf(fpIn, "%s", line);
-			// char* bStr = parser_strtok_r(line, "/", 1);
-			// fscanf(fpIn, "%s", line);
-			// char* cStr = parser_strtok_r(line, "/", 1);
-			// sscanf(aStr, "%d", &a);
-			// sscanf(bStr, "%d", &b);
-			// sscanf(cStr, "%d", &c);
 			sscanf(temp, "%d", verts + i);
 		} else {
 			sscanf(temp, "%d", verts + i);
@@ -227,60 +198,4 @@ FILE *openFile(char *path) {
 		exit(2);
 	}
 	return fpIn;
-}
-
-// Note: This function returns a pointer to a substring of the original string.
-// If the given string was allocated dynamically, the caller must not overwrite
-// that pointer with the returned value, since the original pointer must be
-// deallocated using the same allocator with which it was allocated.  The return
-// value must NOT be deallocated using free() etc.
-char *trimwhitespace(char *str) {
-	char *end;
-
-	// Trim leading space
-	while (isspace((unsigned char)*str))
-		str++;
-
-	if (*str == 0) // All spaces?
-		return str;
-
-	// Trim trailing space
-	end = str + strlen(str) - 1;
-	while (end > str && isspace((unsigned char)*end))
-		end--;
-
-	// Write new null terminator character
-	end[1] = '\0';
-
-	return str;
-}
-
-char *parser_strtok_r(char *str, const char *delim, int newstr) {
-	static int currIndex = 0;
-	if (newstr)
-		currIndex = 0;
-	if (!str || !delim || str[currIndex] == '\0')
-		return NULL;
-
-	char *pS = str;
-	char *W = (char *)malloc(100 * sizeof(char));
-	int i = currIndex, k = 0, j;
-	int br = 0; // bool
-	while (str[i] != '\0' && !br) {
-		j = 0;
-		while (delim[j]) {
-			if (str[i] != delim[j]) {
-				W[k] = str[i];
-				j++;
-				k++;
-			} else {
-				br = 1;
-				break;
-			}
-		}
-		i++;
-	}
-	W[k] = '\0';
-	currIndex = i;
-	return W;
 }
--- RenderEngine/src/parsing/objParser.h
+++ RenderEngine/src/parsing/objParser.h
@@ -6,6 +6,27 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+// helper functions
+
+// Open the path for the given file
+FILE* openFile(char* path);
+
+// Parsing a face line to capture the vertecies of an object
+// Pre:		str - line string
+//		vertN  number of vertecies obtained
+//		verts - the vertecy indexes taht got captured
+// Post: capture and set the proper arguments
+// Returns: void
+void parseFaceLine(char* str, int* vertN, int** verts);
+
+/*
+	Parses an obj file and outputs triangles that is interpretable by the program
+	PRE:	path - path to the .obj file
+			trigList - a pointer to the triangle list
+			size - output int for the number of triangles
+			scale - the scale factor to scale all of the models to be of similar size
+	POST:
+			File is parsed and all of the pre variables are set properly
+	returns: void
+*/
 void parseObj(char *path, Triangle3D **trigList, int *size, float *scale);
-char *parser_strtok_r(char *str, const char *delim, int newstr);
-void centerVertecies(Vector3D* verts);
