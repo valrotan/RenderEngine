a2774709a0fdd9889fe02e3a36d858375173486a
Val <val.rotan@gmail.com>
2020-03-19 22:13:08 -0700

    Coral sample

---
+++ RenderEngine/config/coral.sh
@@ -0,0 +1,16 @@
+../build/engineRun \
+  -i ../input/torus.obj \
+  -o ../output.tga \
+  -i input/coral.obj \
+  -a 3 \
+  -c 5 \
+  -b 0 \
+  -ke .25 \
+  -kd .25 \
+  -ks .6 \
+  -t 0 \
+  -z 2.25 \
+  -w 1920 \
+  -h 1080 \
+  -bkg 000020
+# Model included from turbosquid
