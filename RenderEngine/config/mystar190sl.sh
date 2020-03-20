026575830c8d7aadff48815619c056817f721c0e
Val <val.rotan@gmail.com>
2020-03-19 20:57:28 -0700

    sample

---
+++ RenderEngine/config/mystar190sl.sh
@@ -0,0 +1,3 @@
+../build/engineRun \
+  -i ../input/mystar190sl.obj -my -65 -mx -10 -x -.2 -y -.05 -z 3 -f 30 -bkg 600000 -b .2 -ke .2 -kd .5 -ks 1 -a 4 -t 0 -h 1080 -w 2048
+# Model: https://www.turbosquid.com/3d-models/free-duesen-bayern-mystar-190-3d-model/1062796
